b79a39eeff8f46dc1b5877212d68fa3b117f1e5d
Derek Lim <dereklim3006@gmail.com>
2025-01-03 08:54:52 -0500

    Check for 3-in-a-row and draw; and announce result

--- javascript.js
+++ javascript.js
@@ -32,6 +32,10 @@ function Grid() {
 function GameController() {
   const board = GameBoard()
 
+  let matchOver = false
+
+  const getMatchOver = () => matchOver
+
   const players = [
     {
       name: 'Player 1',
@@ -51,41 +55,64 @@ function GameController() {
 
   const getActivePlayer = () => activePlayer
 
+  const getThreeInRow = () => {
+    const winningLines = [
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]],
+    ]
+
+    for (const line of winningLines) {
+      const [pos1, pos2, pos3] = line
+      const val1 = board.getBoard()[pos1[0]][pos1[1]].getValue()
+      const val2 = board.getBoard()[pos2[0]][pos2[1]].getValue()
+      const val3 = board.getBoard()[pos3[0]][pos3[1]].getValue()
+      if (val1 === val2 && val2 === val3 && (val1 === 'X' || val1 === 'O')) {
+        return line.map(pos => pos.join(''))
+      }
+    }
+    return null
+  }
+
   const playRound = (selectedGrid) => {
     const [row, column] = selectedGrid.split('')
     const grid = document.querySelector(`[data-index='${selectedGrid}']`)
     if (grid.textContent === '') {
       board.placeMarker(row, column, activePlayer.marker)
-      switchPlayerTurn()
+      if (
+        getThreeInRow() ||
+        board.getBoard().flat().every(grid => grid.getValue() !== '')
+      ) {
+        matchOver = true
+      }
+      if (!matchOver) switchPlayerTurn()
     }
   }
 
   return {
     playRound,
     getActivePlayer,
+    getMatchOver,
+    getThreeInRow,
     getBoard: board.getBoard
   }
 }
 
 function ScreenController() {
   const game = GameController()
-  const playerTurnDiv = document.getElementById('turn')
   const boardDiv = document.getElementById('board')
-
+  
   const updateScreen = () => {
-    boardDiv.textContent = ''
-
     const board = game.getBoard()
+    const playerTurnDiv = document.getElementById('turn')
     const activePlayer = game.getActivePlayer()
 
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn`
-    if (activePlayer.name === 'Player 1') {
-      playerTurnDiv.classList.remove('btn-danger')
-      playerTurnDiv.classList.add('btn-primary')
-    } else if (activePlayer.name === 'Player 2') {
-      playerTurnDiv.classList.remove('btn-primary')
-      playerTurnDiv.classList.add('btn-danger')
-    }
+    boardDiv.textContent = ''
 
     board.forEach((row, rowIndex) => {
       row.forEach((grid, colIndex) => {
@@ -96,15 +123,44 @@ function ScreenController() {
         boardDiv.appendChild(gridBtn)
       })
     })
+    
+    if (!game.getMatchOver()) {
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn`
+      if (activePlayer.name === 'Player 1') {
+        playerTurnDiv.classList.remove('btn-danger')
+        playerTurnDiv.classList.add('btn-primary')
+      } else if (activePlayer.name === 'Player 2') {
+        playerTurnDiv.classList.remove('btn-primary')
+        playerTurnDiv.classList.add('btn-danger')
+      }
+    } else {
+      const winningGrids = game.getThreeInRow()
+      if (winningGrids) {
+        winningGrids.forEach(grid => {
+          const gridBtn = document.querySelector(`[data-index='${grid}']`)
+          gridBtn.classList.remove('btn-dark')
+          if (activePlayer.name === 'Player 1') {
+            gridBtn.classList.add('btn-primary')
+          } else {
+            gridBtn.classList.add('btn-danger')
+          }
+        })
+        playerTurnDiv.textContent = `${activePlayer.name} wins!`
+      } else {
+        playerTurnDiv.textContent = 'Draw'
+        playerTurnDiv.classList.remove('btn-primary', 'btn-danger')
+        playerTurnDiv.classList.add('btn-secondary')
+      }
+    }
   }
 
   function clickHandlerBoard(e) {
-    const selectedGrid = e.target.dataset.index
-
-    if (!selectedGrid) return
-
-    game.playRound(selectedGrid)
-    updateScreen()
+    if (!game.getMatchOver()) {
+      const selectedGrid = e.target.dataset.index
+      if (!selectedGrid) return
+      game.playRound(selectedGrid)
+      updateScreen()
+    }
   }
   boardDiv.addEventListener('click', clickHandlerBoard)
 
